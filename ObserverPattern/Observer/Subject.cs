3ad68d4ddf6969b0e8c5e7572d79c5c587f6de04
KPrateek <ks.pratik@gmail.com>
2025-07-18 14:38:11 +0530

    Add files via upload

---
+++ ObserverPattern/Observer/Subject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer
+{
+    class Subject
+    {
+        private int SubjectInteger;
+
+        private List<IObserver> observerList = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            observerList.Add(observer);
+        }
+
+        private void Detach(IObserver observer)
+        {
+            observerList.Remove(observer);
+        }
+
+
+        public void SetData(int i)
+        {
+            SubjectInteger = i;
+            foreach (var observer in observerList)
+            {
+                observer.Update(this);
+            }
+        }
+
+        public int GetData()
+        {
+            return SubjectInteger;
+        }
+
+    }
+}
