a0b631b4097515c973811c8fcc1a6c62203f0420
KPrateek <ks.pratik@gmail.com>
2025-07-18 14:20:21 +0530

    Add files via upload

---
+++ Observer/ConcreteObserver2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer
+{
+    class ConcreteObserver2 : IObserver
+    {
+        public ConcreteObserver2(Subject subject)
+        {
+            subject.Attach(this);
+        }
+
+        public void Update(Object s)
+        {
+            int result = (s as Subject).GetData();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+            ConcreteObserver1 concreteObserver1 = new ConcreteObserver1(subject);
+            ConcreteObserver2 concreteObserver2 = new ConcreteObserver2(subject);
+            subject.SetData(5);
+        }
+    }
+
+
+}
